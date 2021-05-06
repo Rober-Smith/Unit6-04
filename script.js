480840386b5ffa0d43d3d72c5ad2ba504c1c1753
RoberSmith <>
2021-05-06 21:39:30 +0000

    space

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let number = 0
 document.getElementById('calculate').addEventListener('click', buttonOne)
 
 function buttonOne () {
- height = document.getElementById('h').value
- height = parseInt(height)
- base1 = document.getElementById('a').value
- base1 = parseInt(base1)
- base2 = document.getElementById('b').value
- base2 = parseInt(base2)
- number = (base1 + base2) / 2 * height
+  height = document.getElementById('h').value
+  height = parseInt(height)
+  base1 = document.getElementById('a').value
+  base1 = parseInt(base1)
+  base2 = document.getElementById('b').value
+  base2 = parseInt(base2)
+  number = (base1 + base2) / 2 * height
  alert(number)
 }
